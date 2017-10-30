582b8fdf3ecd0329f8a227f4fbc6f54c8e93d147
Mattias <mattias.astrand98@gmail.com>
2017-10-30 15:45:08 +0100

    Adding what was given to us to start developing the project.

---
+++ code/project.cpp
@@ -0,0 +1,13 @@
+#include "tempTrender.h"
+#include <string>
+
+void project() {
+	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets"; //Put the path to your data file here
+	//tempTrender t(pathToFile); //Instantiate your analysis object
+	
+	//t.tempOnDay(8, 23); //Call some functions that you've implemented
+	//t.tempOnDay(235);
+	//t.tempPerDay();
+	//t.hotCold();
+	//t.tempPerYear(2050);
+}
