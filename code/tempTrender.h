92bf47db004e13b144b6b49d22ba6d35bb4141eb
VMLubuntu <none>
2017-10-31 16:17:44 +0100

    We have managed to read the file, and have a test executable for trying out our code.

--- code/tempTrender.h
+++ code/tempTrender.h
@@ -2,12 +2,23 @@
 #define TEMPTRENDER_H
 
 #include <string>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 class tempTrender {
 	public:
 	tempTrender(string filePath); //Construct using the specified file
-	~tempTrender() {} //Destructor
+	~tempTrender();//Destructor
+	string temp;
+	void read_temperatures(){
+		ifstream file(its_filePath.c_str());
+		while(getline(file,temp)){
+			if (getline(file,temp,'9')){
+				cout << temp << endl;
+			}
+		}
+	}
 	
 	//void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
@@ -16,7 +27,7 @@ class tempTrender {
 	//void tempPerYear(int yearToExtrapolate); //Make a histogram of average temperature per year, then fit and extrapolate to the given year
 
 	private:
-	
+	string its_filePath;
 };
 
 #endif
