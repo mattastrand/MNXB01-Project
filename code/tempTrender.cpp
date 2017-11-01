d7cef0505d3db8f89bef3b5fbed1ce3082d994c3
Mattias <mattias.astrand98@gmail.com>
2017-11-01 17:23:37 +0100

    read_temperatures() now works exactly how it should

--- code/tempTrender.cpp
+++ code/tempTrender.cpp
@@ -12,10 +12,10 @@ tempTrender::~tempTrender(){
 }
 
 int main(){
-	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Soderarm.csv"; //Put the path to your data file here
+	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
 	tempTrender t(pathToFile);
 	t.read_temperatures();
-	for ( int i = 0; i < t.data_from_file.size(); i++){
+	for ( int i = 0; i < 30; i++){
 		cout << t.data_from_file.at(i) << endl;
 	} 
 }
--- code/tempTrender.h
+++ code/tempTrender.h
@@ -9,36 +9,32 @@
 using namespace std;
 
 class tempTrender {
+	
 	public:
 	tempTrender(string filePath); //Construct using the specified file
 	~tempTrender();//Destructor
 	
-	//int year, month, day, hour, minute, second;
-	//string separator, control;
-	
 	vector<string> data_from_file; 		 // Will store every meaningful line from the datasets.
-	//vector<string> testvec;
+	
 	vector<string> read_temperatures(){
 	// Let's read a file and store lines with data in a vector.
 		
 		string temp;
-		//float temperature;
 		string input; // Variable that used to fill up the vector.
 		
 		ifstream file(its_filePath.c_str());
 		
 		while(getline(file,temp)){
-			input = temp;
+			// Lines ending with Y (bad recordings) are excluded to begin with.
+			input = temp.substr(0,temp.find(";;",0));
 			size_t pos = input.find("Y");
 			input.erase(0,pos +1);
-			if ((input != "") && (input != ";;.*")){
+			if (input != ""){
 				data_from_file.push_back(input);
 			}
-			//file >> year >> separator >> month >> separator >> day >> separator >> hour >> separator >> minute >> separator >> second >> separator >> temperature >> separator >> control;
-			//cout << year << endl;
 		}
-		// Now let's reshape the vector and remove meaningless information (lines ending with Y = bad, text)
-		data_from_file.erase(data_from_file.begin(), data_from_file.begin() + 12); // Removing first 12 lines, which only include text.
+		// Now let's reshape the vector and remove the first few lines, which only include text.
+		data_from_file.erase(data_from_file.begin(), data_from_file.begin() + 8); 
 		//data_from_file.erase(remove(data_from_file.begin(), data_from_file.end(), 'Y'), data_from_file.end());
 		//cout << data_from_file[11] << endl; // Checking that the above works.
 		return data_from_file;
