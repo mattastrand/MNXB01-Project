0b9b65acf324ea4cbd8f154867548f1871154373
VMLubuntu <none>
2017-11-02 17:24:31 +0100

    Chnages to the reading function

--- code/tempTrender.h
+++ code/tempTrender.h
@@ -2,7 +2,6 @@
 #define TEMPTRENDER_H
 
 #include <string>
-#include <string.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -16,7 +15,7 @@ class tempTrender {
 	
 	
 	vector<string> data_from_file; 		 // Will store every meaningful line from the datasets.
-	
+	vector <float> datavector;
 	vector<string> read_temperatures(){
 	// Let's read a file and store lines with data in a vector.
 		
@@ -29,7 +28,9 @@ class tempTrender {
 				break;
 			}	
 		}
-		
+		float number_of_lines = 0;
+		float yyyy, mm, dd, hour, min, sec, temperature;
+		char d;
 		while(getline(file,temp)){
 			// Lines ending with Y (bad recordings) are excluded to begin with.
 			input = temp.substr(0,temp.find(";;",0));
@@ -37,28 +38,33 @@ class tempTrender {
 			input.erase(0,pos +1);
 			
 			
-			float yyyy, mm, dd, hour, min, sec, temperature;
-			char d;
+			
+			//double data_array [Ayear][Amonth][Aday][Ahour][Atemperature];
+			//int n,m,k;
+			
 			//char key[] = "G";
 			//char buffer[1];
 			//char *var1 = "G";
 			//char e;
 			//cout << e << endl;
+			
+			stringstream data;
 			if(input != ""){
-				stringstream data;
+				//cout << number_of_lines << endl;
+				//++number_of_lines; 
+				
 				data << input;
 				data >> yyyy >> d >> mm >> d >> dd >> d >> hour >> d >> min >> d >> sec >> d >> temperature >> d ;
-				datafile << yyyy << " " << mm << " " << dd << " " << hour << " " << temperature << endl;
-			}
-			if (input != ""){
+				datafile << yyyy << " " << mm << " " << dd << " " << hour << " " << temperature << endl;				
 				data_from_file.push_back(input);
 			}
 		
 		}
 		datafile.close();
+		file.close();
 		// Now let's reshape the vector and remove meaningless information (lines ending with Y = bad, text)
 		//data_from_file.erase(data_from_file.begin(), data_from_file.begin() + 12); // Removing first 12 lines, which only include text.
-		
+		//cout << number_of_lines << endl;
 		
 		return data_from_file;
 		
@@ -69,6 +75,24 @@ class tempTrender {
 	//void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
 	//void tempPerDay(); //Make a histogram of the average temperature of each day of the year
+	
+	//Tentative function
+	vector <float> tempPerDay(int yearToCalculate){
+		ifstream f("relevantdata.dat");
+		string line;
+		vector <float> datavector;
+		while (getline(f, line)){
+			float yyyy, mm, dd, hour, temperature;
+			char d;
+			stringstream data;
+			data << line;
+			data >> yyyy >> d >> mm >> d >> dd >> d >> hour >> d >> temperature;
+			if (yyyy == yearToCalculate){
+				datavector.push_back(temperature);
+			}
+		}
+		return datavector;
+	} 
 	//void hotCold(); //Make a histogram of the hottest and coldest day of the year
 	//void tempPerYear(int yearToExtrapolate); //Make a histogram of average temperature per year, then fit and extrapolate to the given year
 
