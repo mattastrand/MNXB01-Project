dd679c88214ec61e487e8ad608677b1adbe916eb
VMLubuntu <none>
2017-11-04 11:30:56 +0100

    Just some small cleanup

--- code/tempTrender.cpp
+++ code/tempTrender.cpp
@@ -16,84 +16,82 @@ tempTrender::~tempTrender(){
 
 
 void testFunc(){
-	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Soderarm.csv"; //Put the path to your data file here
-	string path2 = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lulea.csv";
-	tempTrender s(pathToFile);
-	s.read_temperatures();
-	//s.tempPerDay(1973);
-	s.tempPerDayExtended();
-	/*double xAxis[s.datavector.size()], yAxis[s.datavector.size()];
-	for(unsigned int i = 0; i < s.datavector.size(); i++){
-		xAxis[i] = i;
-		yAxis[i] = s.datavector.at(i);
-	}*/
+	
+	//Define the canvas and legend to be used
+	TCanvas* c1 = new TCanvas("c1", "hPhi canvas", 900, 600);
+	TLegend *leg = new TLegend(0.50,0.5,0.75, 0.3);
+	
+	//Below is how the histogram for any dataset is constructed. 
+	
+	
+	string pathSoder = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Soderarm.csv"; //Put the path to your data file here
+	tempTrender Soder(pathSoder);
+	Soder.read_temperatures();
+	//soder.tempPerDay(1973);
+	Soder.tempPerDayExtended();
 
-	//TGraph* gr = new TGraph(s.datavector.size(), xAxis, yAxis);
+	TH1D* SoderHist = new TH1D("hPhi", "Soderarm; Day; Temperature", 
+			Soder.avgOfData.size(), 0, Soder.avgOfData.size());
 	
-	TH1D* hist = new TH1D("hPhi", "Söderarm; Day; Temperature", 
-			s.avgOfData.size(), 0, s.avgOfData.size());
-	
-	for(unsigned int i = 0 ; i< s.avgOfData.size() ; i++){
-		hist->SetBinContent(i,s.avgOfData.at(i));
+	for(unsigned int i = 0 ; i< Soder.avgOfData.size() ; i++){
+		SoderHist->SetBinContent(i,Soder.avgOfData.at(i));
 	}	
 	
-	TCanvas* c1 = new TCanvas("c1", "hPhi canvas", 900, 600);
+	leg->AddEntry(SoderHist, "", "F");
+	SoderHist->SetLineColor(35);
+	SoderHist->SetMinimum(-15);
+	SoderHist->SetMaximum(22),
+	SoderHist->Draw();
 	
 	
-	string path3 = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv";
-	tempTrender Lund(path3);
+	
+	string pathLund = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv";
+	tempTrender Lund(pathLund);
 	Lund.read_temperatures();
 	//Lund.tempPerDay(1973);
 	Lund.tempPerDayExtended();
 	
-	TH1D* Lhist = new TH1D("hPhi", "Lund; phi; Counts", 
+	TH1D* LundHist = new TH1D("hPhi", "Lund; phi; Counts", 
 			Lund.avgOfData.size(), 0, Lund.avgOfData.size());
+	
 	for (unsigned int i = 0; i < Lund.avgOfData.size(); i++){
-		Lhist->SetBinContent(i,Lund.avgOfData.at(i));
+		LundHist->SetBinContent(i,Lund.avgOfData.at(i));
 	}
-	tempTrender Lule(path2);
+	
+	leg->AddEntry(LundHist, "", "F");
+	LundHist->SetLineColor(24);
+	LundHist->Draw("SAME");
+	
+	
+	
+	string pathLule = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lulea.csv";
+	tempTrender Lule(pathLule);
 	Lule.read_temperatures();
 	Lule.tempPerDayExtended();
-	TH1D* Luhist = new TH1D("hPhi", "Luleå; phi; Counts", 
+	
+	TH1D* LuleHist = new TH1D("hPhi", "Lulea; phi; Counts", 
 			Lule.avgOfData.size(), 0, Lule.avgOfData.size());
+	
 	for (unsigned int i = 0; i < Lule.avgOfData.size(); i++){
-		Luhist->SetBinContent(i,Lule.avgOfData.at(i));
+		LuleHist->SetBinContent(i,Lule.avgOfData.at(i));
 	}
-	//gr->GetXaxis()->SetRangeUser(0,s.datavector.size());
-	//gr->Draw();
-	TLegend *leg = new TLegend(0.50,0.5,0.75, 0.3);
-	leg->AddEntry(hist, "", "F");
-	leg->AddEntry(Luhist, "", "F");
-	leg->AddEntry(Lhist, "", "F");
 	
-	hist->SetLineColor(35);
-	hist->SetMinimum(-10);
-	hist->SetMaximum(22),
-	hist->Draw();
+	leg->AddEntry(LuleHist, "", "F");
+	LuleHist->SetLineColor(12);
+	LuleHist->Draw("SAME");
 	
-	Lhist->SetLineColor(24);
-	Lhist->Draw("SAME");
 	
-	Luhist->SetLineColor(12);
-	Luhist->Draw("SAME");
+	
 	leg->Draw();
-	//cout << s.datavector.size() << endl;
 }
 
 int main(){
+	/*
+	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
 
-	string pathToFile = "/home/magnus/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
-
-	//string pathToFile;
-	//cout << "gimme the file" << endl;
-	//cin >> pathToFile;
 	tempTrender t(pathToFile);
 	t.read_temperatures();
 	
-	
-	//for ( int i = 0; i < 30; i++){
-	//	cout << t.data_from_file.at(i) << endl;
-	//} 
 	
 	t.tempPerDay(1973);
 	cout <<t.datavector.size() << endl;
@@ -101,7 +99,5 @@ int main(){
 		cout << t.datavector.at(i) << endl;
 	}
 	t.tempPerDayExtended();
-	//cout << t.datavector.size() << endl;
-	//t.testFunc();
-
+	*/
 }
--- code/tempTrender.h
+++ code/tempTrender.h
@@ -29,36 +29,40 @@ class tempTrender {
 	// Let's read a file and store lines with data in a vector and sort them then store them in a new datafile.
 		
 
-		
+		//These variables are used with stringstream later on to extract the relevant data from the raw data.
 		float yyyy, mm, dd, hour, min, sec, temperature;
 		char d;
 
-		string input, temp, useless_line; // Variables that are used to fill up the vector.
+		// Variables that are used to fill up the vector.
+		string input, temp, useless_line; 
 		
 		ifstream file(its_filePath.c_str());
 		ofstream datafile("relevantdata.dat");
 		
 		//Skipping the first few lines, which only contain text.
 		while(getline(file,useless_line)){
+			
 			if (useless_line == "Datum;Tid (UTC);Lufttemperatur;Kvalitet;;Tidsutsnitt:"){
 				break;
 			}	
 		}
 		
-		float number_of_lines = 0; // In case we want to count the lines in the file.
-		
 		while(getline(file,temp)){
-			// Lines ending with Y (bad recordings) are excluded to begin with.
+			
+			// Lines are split up into substrings such that the last part of some lines is removed.
+			// Then lines with Y (bad recordings) are excluded by identifying those and erasing the line.
 			input = temp.substr(0,temp.find(";;",0));
 			size_t pos = input.find("Y");
 			input.erase(0,pos +1);
 			
+			//Using stringstream below, the raw data is split up and the relevant parts are extracted.
 			stringstream data;
+			
+			//This is to only feed non-empty lines into the .dat file.
 			if(input != ""){
 				
-				//cout << number_of_lines << endl;
-				//++number_of_lines; 
-				
+				//The data here is segmented into the format given in the raw data files, to allow for easy extraction.
+				//d sipmly signifies the delimiter between the numbers in the files.
 				data << input;
 				data >> yyyy >> d >> mm >> d >> dd >> d >> hour >> d >> min >> d >> sec >> d >> temperature >> d ;
 				datafile << yyyy << " " << mm << " " << dd << " " << hour << " " << temperature << endl;				
@@ -73,9 +77,15 @@ class tempTrender {
 		
 	}
 	
+	
+	
+	
 	//void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
 	//void tempPerDay(); //Make a histogram of the average temperature of each day of the year
+	
+	
+	
 	
 	//Tentative function
 	vector <float> tempPerDay(int yearToCalculate){
@@ -83,24 +93,34 @@ class tempTrender {
 		
 		ifstream f("relevantdata.dat"); // Opening the organized datafile produced in read_temperatures().
 		string line; 
+		
+		
 		// Variables used to fill a datavector (in turn used to produce the histogram) with mean temperatures corresponding to each day in the chosen year:
 		float yyyy, mm, dd, hour, temperature, mean_temp, tot_temp = 0; 
-		int old_day = 0, measurementNo = 0; // Don't know what the first day in the file is. Give old_day a "wrong" initial value, then give it the day value read in each new line.
+		
+		// Don't know what the first day in the file is. Give old_day a "wrong" initial value, then give it the day value read in each new line.
+		int old_day = 0, measurementNo = 0; 
 		char d;
 		
 		while (getline(f, line)){
 			// The while loop goes through every line in the datafile. It adds up temperatures until a new day is reached, then takes the mean of these and outputs it in the datavector.
+			
+			
 			stringstream data;
 			data << line;
 			data >> yyyy >> mm >> dd >> hour >> temperature;
 			
 			cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
+			
+			
 			if ((yyyy == yearToCalculate) || (yyyy == yearToCalculate + 1)){ // Checking that we are in the right year. year+1 condition is needed for the las day of the year.
+				
 				if ((old_day == dd) || (old_day == 0)){ // As long as the day is the same as in the previous line, add the temperature to a temporary total.
 					tot_temp += temperature;
 					measurementNo += 1;
 					old_day = dd;
 				}
+				
 				else {
 					mean_temp = tot_temp / measurementNo; // When the day changes, divide the (temporary) total by the corresponding number of read lines.
 					datavector.push_back(mean_temp); // Store the mean in the datavector.
@@ -110,9 +130,12 @@ class tempTrender {
 				}
 				
 			}
+			
+			
 			if ( yyyy == yearToCalculate + 1){ // Kill the above process once a new year is reached.
 				break;
 			}
+			
 			f.close();
 		}
 		
@@ -150,10 +173,17 @@ class tempTrender {
 	}*/
 
 	
-
+	//This function is used to calculate the average temperature of every day over all years. 
 	void tempPerDayExtended(){
-	
+		
+		//The vectors below are filled with the total temperature of any given day
+		//and the number of measurements on that day appears in the entire dataset.
+		//The day is counted as the number of days since the start of the year.
+		//This means that February 29th appears in the 60th position every four years,
+		//and every other day is pushed back, so that the last entry is December 31st every four years. 
 		vector <float> sumOfData(366,0), countsOfData(366,0);
+		
+		
 		for(int k=0; k<=366; k++){
 			avgOfData.push_back(0);			
 		}
@@ -161,6 +191,7 @@ class tempTrender {
 		ifstream file("relevantdata.dat");
 		string line;
 		
+		//As this function runs over every year, it handles the change of year in the same
 		float temperature, mean_temp, tot_temp = 0;
 		int old_day = 0, old_year=0, measurementNo = 0, start=0, dayCount=0, yyyy, mm, dd, hour; 
 		cout << "start " << start << endl;
