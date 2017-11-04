40336cd03bc07305eba3ec809267a8302baa1752
VMLubuntu <none>
2017-11-04 14:10:45 +0100

    Changed the code to work with the Uppsala data set too

--- code/tempTrender.cpp
+++ code/tempTrender.cpp
@@ -22,74 +22,154 @@ void testFunc(){
 	//Below is how the histogram for any dataset is constructed. 
 	
 	
-	string pathSoder = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Soderarm.csv"; //Put the path to your data file here
-	tempTrender Soder(pathSoder);
-	Soder.read_temperatures();
-	//soder.tempPerDay(1973);
-	Soder.tempPerDayExtended();
+	
+	//string pathSoder = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Soderarm.csv"; //Put the path to your data file here
+	//tempTrender Soder(pathSoder);
+	//Soder.read_temperatures();
+	//Soder.tempPerDay(1973);
+	
+	//TH1D* SoderHist = new TH1D("hPhi", "Soderarm; Day; Temperature", 
+			//Soder.datavector.size(), 0, Soder.datavector.size());
+	
+	//for(unsigned int i = 0 ; i< Soder.datavector.size() ; i++){
+		//SoderHist->SetBinContent(i,Soder.datavector.at(i));
+	//}
+	
+	
+	//Soder.tempPerDayExtended();
 
-	TH1D* SoderHist = new TH1D("hPhi", "Soderarm; Day; Temperature", 
-			Soder.avgOfData.size(), 0, Soder.avgOfData.size());
+	//TH1D* SoderHist = new TH1D("hPhi", "Soderarm; Day; Temperature", 
+			//Soder.avgOfData.size(), 0, Soder.avgOfData.size());
 	
-	for(unsigned int i = 0 ; i< Soder.avgOfData.size() ; i++){
-		SoderHist->SetBinContent(i,Soder.avgOfData.at(i));
+	//for(unsigned int i = 0 ; i< Soder.avgOfData.size() ; i++){
+		//SoderHist->SetBinContent(i,Soder.avgOfData.at(i));
 
 	
 
-	}	
+	//}
 	
-	leg->AddEntry(SoderHist, "", "F");
-	SoderHist->SetLineColor(35);
-	SoderHist->SetMinimum(-15);
-	SoderHist->SetMaximum(22),
-	SoderHist->Draw();
+	//leg->AddEntry(SoderHist, "", "F");
+	//SoderHist->SetLineColor(35);
+	//SoderHist->SetMinimum(-20);
+	//SoderHist->SetMaximum(30),
+	//SoderHist->Draw();
 	
 	
 	
+	string pathUpp = "/home/courseuser/MNXB01/2017HT/Project/datasets/uppsala_tm_1722-2013.dat";
+	tempTrender Upp(pathUpp);
+	
+	Upp.read_temperatures();
+	
+	Upp.tempPerDayExtended();
+	
+	TH1D* UppHist = new TH1D ("hPhi", "Uppsala; Day; Temperature", 
+			Upp.avgOfData.size(), 0, Upp.avgOfData.size());
+			
+	for(unsigned int i = 0 ; i< Upp.avgOfData.size() ; i++){
+		UppHist->SetBinContent(i,Upp.avgOfData.at(i));
+	}
+	
+	leg->AddEntry(UppHist, "", "F");
+	UppHist->SetLineColor(35);
+	UppHist->SetMinimum(-20);
+	UppHist->SetMaximum(30),
+	UppHist->Draw();
+	
+	TH1D* UrbanUppHist = new TH1D ("hPhi", "Uppsala Urban; Day; Temperature", 
+			Upp.urbanAvgOfData.size(), 0, Upp.urbanAvgOfData.size());
+			
+	for(unsigned int i = 0 ; i< Upp.urbanAvgOfData.size() ; i++){
+		UrbanUppHist->SetBinContent(i,Upp.urbanAvgOfData.at(i));
+	}
+	
+	leg->AddEntry(UrbanUppHist, "", "F");
+	UrbanUppHist->Draw("SAME");
+	leg->Draw();
+	
+	//string pathLund = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv";
+	//tempTrender Lund(pathLund);
 
-	string pathLund = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv";
-	tempTrender Lund(pathLund);
-
-	Lund.read_temperatures();
+	//Lund.read_temperatures();
 
 	//Lund.tempPerDay(1973);
-	Lund.tempPerDayExtended();
 	
-	TH1D* LundHist = new TH1D("hPhi", "Lund; phi; Counts", 
-			Lund.avgOfData.size(), 0, Lund.avgOfData.size());
 	
-	for (unsigned int i = 0; i < Lund.avgOfData.size(); i++){
-		LundHist->SetBinContent(i,Lund.avgOfData.at(i));
-	}
+	//TH1D* LundHist = new TH1D("hPhi", "Lund; Day; Temperature", 
+			//Lund.datavector.size(), 0, Lund.datavector.size());
 	
-	leg->AddEntry(LundHist, "", "F");
-	LundHist->SetLineColor(24);
-	LundHist->Draw("SAME");
+	//for(unsigned int i = 0 ; i< Lund.datavector.size() ; i++){
+		//LundHist->SetBinContent(i,Lund.datavector.at(i));
+	//}
 	
 	
 	
-	string pathLule = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lulea.csv";
-	tempTrender Lule(pathLule);
-	Lule.read_temperatures();
-	Lule.tempPerDayExtended();
 	
-	TH1D* LuleHist = new TH1D("hPhi", "Lulea; phi; Counts", 
-			Lule.avgOfData.size(), 0, Lule.avgOfData.size());
+	//Lund.tempPerDayExtended();
 	
-	for (unsigned int i = 0; i < Lule.avgOfData.size(); i++){
-		LuleHist->SetBinContent(i,Lule.avgOfData.at(i));
-	}
+	//TH1D* LundHist = new TH1D("hPhi", "Lund; phi; Counts", 
+			//Lund.avgOfData.size(), 0, Lund.avgOfData.size());
+	
+	//for (unsigned int i = 0; i < Lund.avgOfData.size(); i++){
+		//LundHist->SetBinContent(i,Lund.avgOfData.at(i));
+	//}
+	
+	//leg->AddEntry(LundHist, "", "F");
+	//LundHist->SetLineColor(24);
+	//LundHist->Draw("SAME");
+	
+	
+	
+	//string pathLule = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lulea.csv";
+	//tempTrender Lule(pathLule);
+	//Lule.read_temperatures();
+	
+	//Lule.tempPerDay(1973);
+	
+	//TH1D* LuleHist = new TH1D("hPhi", "Lulea; Day; Temperature", 
+			//Lule.datavector.size(), 0, Lule.datavector.size());
+	
+	//for(unsigned int i = 0 ; i< Lule.datavector.size() ; i++){
+		//LuleHist->SetBinContent(i,Lule.datavector.at(i));
+	//}
+	
+	
+	//Lule.tempPerDayExtended();
+	
+	//TH1D* LuleHist = new TH1D("hPhi", "Lulea; phi; Counts", 
+			//Lule.avgOfData.size(), 0, Lule.avgOfData.size());
+	
+	//for (unsigned int i = 0; i < Lule.avgOfData.size(); i++){
+		//LuleHist->SetBinContent(i,Lule.avgOfData.at(i));
+	//}
 		
-	leg->AddEntry(LuleHist, "", "F");
-	LuleHist->SetLineColor(12);
-	LuleHist->Draw("SAME");
+	//leg->AddEntry(LuleHist, "", "F");
+	//LuleHist->SetLineColor(12);
+	//LuleHist->Draw("SAME");
 	
 	
 	
-	leg->Draw();
+	//leg->Draw();
+	
 }
 
 int main(){
+	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/uppsala_tm_1722-2013.dat";
+	tempTrender Upp(pathToFile);
+	Upp.read_temperatures();
+	Upp.tempPerDayExtended();
+	//Upp.tempPerDay(1973);
+	/*for (unsigned int i = 0; i< Upp.datavector.size();i++){
+		cout << Upp.datavector.at(i) << endl;
+		cout << Upp.urbanDatavector.at(i) << endl;
+	}*/
+	/*string pathLule = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lulea.csv";
+	tempTrender Lule(pathLule);
+	Lule.read_temperatures();
+	Lule.tempPerDay(1973);
+	for (unsigned int i = 0 ; i< Lule.datavector.size(); i++){
+		cout << Lule.datavector.at(i) << endl;
+	}*/
 	/*
 	string pathToFile = "/home/courseuser/MNXB01/2017HT/Project/datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
 
--- code/tempTrender.h
+++ code/tempTrender.h
@@ -23,52 +23,67 @@ class tempTrender {
 	~tempTrender();//Destructor
 	
 	vector<string> data_from_file; // Will store every meaningful line from the datasets.
-	vector <float> datavector,avgOfData;
+	vector <float> datavector,urbanDatavector,avgOfData, urbanAvgOfData;
 	
 	vector<string> read_temperatures(){
 	// Let's read a file and store lines with data in a vector and sort them then store them in a new datafile.
 		
-
 		//These variables are used with stringstream later on to extract the relevant data from the raw data.
-		float yyyy, mm, dd, hour, min, sec, temperature;
+		float yyyy, mm, dd, hour, min, sec, temperature,urbanTemp, control;
 		char d;
-
+		
 		// Variables that are used to fill up the vector.
 		string input, temp, useless_line; 
 		
 		ifstream file(its_filePath.c_str());
-		ofstream datafile("relevantdata.dat");
-		
-		//Skipping the first few lines, which only contain text.
-		while(getline(file,useless_line)){
+		ofstream datafile("relevantdata.dat"); 
+		 
+		 
+		if(its_filePath == "/home/courseuser/MNXB01/2017HT/Project/datasets/uppsala_tm_1722-2013.dat"){
 			
-			if (useless_line == "Datum;Tid (UTC);Lufttemperatur;Kvalitet;;Tidsutsnitt:"){
-				break;
-			}	
-		}
-		
-		while(getline(file,temp)){
 			
-			// Lines are split up into substrings such that the last part of some lines is removed.
-			// Then lines with Y (bad recordings) are excluded by identifying those and erasing the line.
-			input = temp.substr(0,temp.find(";;",0));
-			size_t pos = input.find("Y");
-			input.erase(0,pos +1);
-			
-			//Using stringstream below, the raw data is split up and the relevant parts are extracted.
-			stringstream data;
-			
-			//This is to only feed non-empty lines into the .dat file.
-			if(input != ""){
-				
-				//The data here is segmented into the format given in the raw data files, to allow for easy extraction.
-				//d sipmly signifies the delimiter between the numbers in the files.
-				data << input;
-				data >> yyyy >> d >> mm >> d >> dd >> d >> hour >> d >> min >> d >> sec >> d >> temperature >> d ;
-				datafile << yyyy << " " << mm << " " << dd << " " << hour << " " << temperature << endl;				
-				data_from_file.push_back(input);
+			while(getline(file,temp)){
+				stringstream data;
+				data << temp;
+				data >> yyyy >> mm >> dd >> temperature >> urbanTemp >> control;
+				if (control == 1){
+					datafile << yyyy << " " << mm << " " << dd << " " << temperature << " " << urbanTemp << endl;
+				}
 			}
+		}		
 		
+		else {
+			//Skipping the first few lines, which only contain text.
+			while(getline(file,useless_line)){
+				
+				if (useless_line == "Datum;Tid (UTC);Lufttemperatur;Kvalitet;;Tidsutsnitt:"){
+					break;
+				}	
+			}
+			
+			while(getline(file,temp)){
+				
+				// Lines are split up into substrings such that the last part of some lines is removed.
+				// Then lines with Y (bad recordings) are excluded by identifying those and erasing the line.
+				input = temp.substr(0,temp.find(";;",0));
+				size_t pos = input.find("Y");
+				input.erase(0,pos +1);
+				
+				//Using stringstream below, the raw data is split up and the relevant parts are extracted.
+				stringstream data;
+				
+				//This is to only feed non-empty lines into the .dat file.
+				if(input != ""){
+					
+					//The data here is segmented into the format given in the raw data files, to allow for easy extraction.
+					//d sipmly signifies the delimiter between the numbers in the files.
+					data << input;
+					data >> yyyy >> d >> mm >> d >> dd >> d >> hour >> d >> min >> d >> sec >> d >> temperature >> d ;
+					datafile << yyyy << " " << mm << " " << dd << " " << hour << " " << temperature << endl;				
+					data_from_file.push_back(input);
+				}
+			
+			}
 		}
 		datafile.close();
 		file.close();
@@ -96,52 +111,107 @@ class tempTrender {
 		
 		
 		// Variables used to fill a datavector (in turn used to produce the histogram) with mean temperatures corresponding to each day in the chosen year:
-		float yyyy, mm, dd, hour, temperature, mean_temp, tot_temp = 0; 
+		float temperature, urbanTemp, mean_temp, urbanMean_temp, tot_temp = 0, urbanTot_temp =0; 
 		
 		// Don't know what the first day in the file is. Give old_day a "wrong" initial value, then give it the day value read in each new line.
-		int old_day = 0, measurementNo = 0; 
+		int old_day = 0, old_year=0, measurementNo = 0, start=0, yyyy, mm, dd, hour, old_month=0;
 		char d;
 		
-		while (getline(f, line)){
-			// The while loop goes through every line in the datafile. It adds up temperatures until a new day is reached, then takes the mean of these and outputs it in the datavector.
-			
-			
-			stringstream data;
-			data << line;
-			data >> yyyy >> mm >> dd >> hour >> temperature;
-			
-			cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
-			
-			
-			if ((yyyy == yearToCalculate) || (yyyy == yearToCalculate + 1)){ // Checking that we are in the right year. year+1 condition is needed for the las day of the year.
-				
-				if ((old_day == dd) || (old_day == 0)){ // As long as the day is the same as in the previous line, add the temperature to a temporary total.
-					tot_temp += temperature;
-					measurementNo += 1;
-					old_day = dd;
-				}
-				
-				else {
-					mean_temp = tot_temp / measurementNo; // When the day changes, divide the (temporary) total by the corresponding number of read lines.
-					datavector.push_back(mean_temp); // Store the mean in the datavector.
-					old_day = dd; 
-					measurementNo = 1; // Reset (temporary) temperature total and corresponding number of read lines.
-					tot_temp = temperature;
-				}
-				
-			}
-			
-			
-			if ( yyyy == yearToCalculate + 1){ // Kill the above process once a new year is reached.
-				break;
-			}
-			
-			f.close();
+		int number_of_days = ::keepTrack(yearToCalculate, 12, 31);
+		
+		for(int k=0; k<number_of_days; k++){
+			datavector.push_back(0);	
+			urbanDatavector.push_back(0);	
 		}
 		
-		return datavector;
+		if(its_filePath == "/home/courseuser/MNXB01/2017HT/Project/datasets/uppsala_tm_1722-2013.dat"){
+			
+			
+			while (getline(f, line)){
+				stringstream data;
+				data << line;
+				data >> yyyy >> mm >> dd >> temperature >> urbanTemp;
+			
+				//cout << "uppsala" << endl;
+				
+				if ((yyyy == yearToCalculate) || (yyyy == yearToCalculate + 1)){ // Checking that we are in the right year. year+1 condition is needed for the las day of the year.
+					//cout << yyyy << endl;
+					if ((old_day == dd) || (old_day == 0)){ // As long as the day is the same as in the previous line, add the temperature to a temporary total.
+						tot_temp += temperature;
+						//cout << tot_temp << endl;
+						urbanTot_temp += urbanTemp;
+						//cout << urbanTot_temp << endl;
+						measurementNo += 1;
+						old_day = dd;
+						old_month = mm;
+					}
+					
+					else {
+						mean_temp = tot_temp / measurementNo; // When the day changes, divide the (temporary) total by the corresponding number of read lines.
+						urbanMean_temp = urbanTot_temp /measurementNo;
+						datavector[::keepTrack(yearToCalculate, old_month,old_day) -1]=mean_temp; // Store the mean in the datavector.
+						urbanDatavector[::keepTrack(yearToCalculate, old_month, old_day) -1] = urbanMean_temp;
+						old_day = dd; 
+						old_month = mm;
+						measurementNo = 1; // Reset (temporary) temperature total and corresponding number of read lines.
+						tot_temp = temperature;
+						urbanTot_temp = urbanTemp;
+					}
+					
+				}
+				
+				
+				if ( yyyy == yearToCalculate + 1){ // Kill the above process once a new year is reached.
+					break;
+				}
+					
+			}
+		}
+		
+		else{
+			
+			while (getline(f, line)){
+				// The while loop goes through every line in the datafile. It adds up temperatures until a new day is reached, then takes the mean of these and outputs it in the datavector.
+				
+				
+				stringstream data;
+				data << line;
+				data >> yyyy >> mm >> dd >> hour >> temperature;
+				//cout << "notupp" << endl;
+				//cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
+				
+				if ((yyyy == yearToCalculate) || (yyyy == yearToCalculate + 1)){ // Checking that we are in the right year. year+1 condition is needed for the las day of the year.
+					
+					if ((old_day == dd) || (old_day == 0)){ // As long as the day is the same as in the previous line, add the temperature to a temporary total.
+						tot_temp += temperature;
+						//cout << tot_temp << endl;
+						measurementNo += 1;
+						old_day = dd;
+						old_month = mm;
+					}
+					
+					else {
+						mean_temp = tot_temp / measurementNo; // When the day changes, divide the (temporary) total by the corresponding number of read lines.
+						datavector[::keepTrack(yearToCalculate, old_month,old_day) -1]=mean_temp; // Store the mean in the datavector.
+						old_day = dd; 
+						old_month = mm;
+						measurementNo = 1; // Reset (temporary) temperature total and corresponding number of read lines.
+						tot_temp = temperature;
+					}
+					
+				}
+				
+				
+				if ( yyyy == yearToCalculate + 1){ // Kill the above process once a new year is reached.
+					break;
+				}
+				
+			}	
+			f.close();
+		}
+		return datavector, urbanDatavector;
 
-	} 
+	}
 	
 	//void hotCold(); //Make a histogram of the hottest and coldest day of the year
 	//void tempPerYear(int yearToExtrapolate); //Make a histogram of average temperature per year, then fit and extrapolate to the given year
@@ -181,82 +251,164 @@ class tempTrender {
 		//This means that February 29th appears in the 60th position every four years,
 		//and every other day is pushed back, so that the last entry is December 31st every four years. 
 
-		vector <float> sumOfData(366,0), countsOfData(366,0);
+		vector <float> sumOfData(366,0), countsOfData(366,0), urbanSumOfData(366,0);
 		
 		
-		for(int k=0; k<=366; k++){
-			avgOfData.push_back(0);			
+		for(int k=0; k<366; k++){
+			avgOfData.push_back(0);	
+			urbanAvgOfData.push_back(0);		
 		}
 		
 		ifstream file("relevantdata.dat");
 		string line;
 		
 		//As this function runs over every year, it handles the change of year in the same
-		float temperature, mean_temp, tot_temp = 0;
+		float temperature, mean_temp, tot_temp = 0, urbanTemp, urbanMean_temp, urbanTot_temp = 0;
 		int old_day = 0, old_year=0, measurementNo = 0, start=0, yyyy, mm, dd, hour, old_month=0; 
 		cout << "start " << start << endl;
-		while(getline(file,line)){
-			
-			stringstream data;
-			data << line;
-			data >> yyyy >> mm >> dd >> hour >> temperature;
-			
-			if(old_day==dd && start==1){
-				old_day=dd;
-				tot_temp +=temperature, 
-				measurementNo+=1;
+		
+		if(its_filePath == "/home/courseuser/MNXB01/2017HT/Project/datasets/uppsala_tm_1722-2013.dat"){
+		
+			while(getline(file,line)){
+				
+				stringstream data;
+				data << line;
+				data >> yyyy >> mm >> dd >> temperature >> urbanTemp;
+				
+				if(old_day==dd && start==1){
+					old_day=dd;
+					tot_temp +=temperature; 
+					urbanTot_temp += urbanTemp;
+					measurementNo+=1;
+					
+					
+				}
+				
+				else if((old_year+1)==yyyy && start==1){
+					
+					
+					mean_temp=(tot_temp)/measurementNo;
+					urbanMean_temp = (urbanTot_temp)/measurementNo;
+					sumOfData[::keepTrack(old_year,old_month,old_day)-1]+=mean_temp;
+					urbanSumOfData[::keepTrack(old_year,old_month,old_day)-1]+=urbanMean_temp;
+					countsOfData[::keepTrack(old_year,old_month, old_day)-1]+=1;	
+					
+					tot_temp=temperature;
+					urbanTot_temp=urbanTemp;
+					old_day=dd;
+					old_month=mm;
+					old_year=yyyy;
+					measurementNo=1;
+					
+					
+				}
 				
 				
+				else if(start==0){
+					
+					old_day=dd;
+					old_month=mm;
+					old_year=yyyy;
+					measurementNo=1;
+					tot_temp=temperature;
+					urbanTot_temp=urbanTemp;
+					cout << "Start " << start << endl;
+					//cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
+					start=1;
+				}
+				
+				else{
+					
+					mean_temp=(tot_temp)/measurementNo;
+					urbanMean_temp = (urbanTot_temp)/measurementNo;
+					sumOfData[::keepTrack(old_year,old_month, old_day)-1]+=mean_temp;
+					urbanSumOfData[::keepTrack(old_year,old_month,old_day)-1]+=urbanMean_temp;
+					countsOfData[::keepTrack(old_year,old_month,old_day)-1]+=1;
+					
+					tot_temp=temperature;
+					urbanTot_temp=urbanTemp;
+					old_day=dd;
+					old_month=mm;
+					measurementNo=1;			
+					
+				}
+			
 			}
 			
-			else if((old_year+1)==yyyy && start==1){
-				
-				
-				mean_temp=(tot_temp)/measurementNo;
-				sumOfData[::keepTrack(old_year,old_month,old_day)-1]+=mean_temp;
-				countsOfData[::keepTrack(old_year,old_month, old_day)-1]+=1;	
-				
-				tot_temp=temperature;
-				old_day=dd;
-				old_month=mm;
-				old_year=yyyy;
-				measurementNo=1;
-				
-				
-			}
 			
-			
-			else if(start==0){
-				
-				old_day=dd;
-				old_month=mm;
-				old_year=yyyy;
-				measurementNo=1;
-				tot_temp=temperature;
-				cout << "Start " << start << endl;
-				cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
-				start=1;
-			}
-			
-			else{
-				
-				mean_temp=(tot_temp)/measurementNo;
-				sumOfData[::keepTrack(old_year,old_month, old_day)-1]+=mean_temp;
-				countsOfData[::keepTrack(old_year,old_month,old_day)-1]+=1;
-				
-				tot_temp=temperature;
-				old_day=dd;
-				old_month=mm;
-				measurementNo=1;			
-				
-			}
+			file.close();
 		
 		}
-		file.close();
+		
+		else{ 
+			
+			while(getline(file,line)){
+				
+				stringstream data;
+				data << line;
+				data >> yyyy >> mm >> dd >> hour >> temperature;
+				
+				if(old_day==dd && start==1){
+					old_day=dd;
+					tot_temp +=temperature;
+					measurementNo+=1;
+					
+					
+				}
+				
+				else if((old_year+1)==yyyy && start==1){
+					
+					
+					mean_temp=(tot_temp)/measurementNo;
+					sumOfData[::keepTrack(old_year,old_month,old_day)-1]+=mean_temp;
+					countsOfData[::keepTrack(old_year,old_month, old_day)-1]+=1;	
+					
+					tot_temp=temperature;
+					old_day=dd;
+					old_month=mm;
+					old_year=yyyy;
+					measurementNo=1;
+					
+					
+				}
+				
+				
+				else if(start==0){
+					
+					old_day=dd;
+					old_month=mm;
+					old_year=yyyy;
+					measurementNo=1;
+					tot_temp=temperature;
+					cout << "Start " << start << endl;
+					cout << yyyy <<" "<< mm <<" "<< dd <<" "<< hour <<" " << temperature << endl;
+					start=1;
+				}
+				
+				else{
+					
+					mean_temp=(tot_temp)/measurementNo;
+					sumOfData[::keepTrack(old_year,old_month, old_day)-1]+=mean_temp;
+					countsOfData[::keepTrack(old_year,old_month,old_day)-1]+=1;
+					
+					tot_temp=temperature;
+					old_day=dd;
+					old_month=mm;
+					measurementNo=1;			
+					
+				}
+			
+			}
+			
+			
+			file.close();
+			
+		}
 		for(int j=0; j<=365; j++){
 			avgOfData[j]=(sumOfData[j])/countsOfData[j];
-			cout << avgOfData[j] << endl;	
-	
+			urbanAvgOfData[j]=(urbanSumOfData[j])/countsOfData[j];
+			//cout << avgOfData[j] << endl;
+			cout << urbanAvgOfData[j] << endl;		
 		}
 
 		
